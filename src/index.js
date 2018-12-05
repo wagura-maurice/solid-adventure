4f550c3847017b0caa8815f2da9ed83514d6262e
Wagura Maurice <wagura465@gmail.com>
2018-12-05 12:59:14 +0300

    added popper 1.0.1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render (<App />, document.getElementById ('root'));
 
